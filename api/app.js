297047a04599baa8f44c6cddeee07318626f26b4
Jerry Leung <luongvidu@gmail.com>
2021-10-02 21:25:08 +1000

    Prepare for deployment

--- api/app.js
+++ api/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const compression = require('compression');
+const path = require('path');
 //Routers
 const productRoute = require('./routes/product.route');
 const customerRoute = require('./routes/customer.route');
@@ -14,12 +16,23 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json());
+app.use(compression());
 
 app.use('/api/v1/products', productRoute);
 app.use('/api/v1/customers', customerRoute);
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
-});
+const __currentPath = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+  console.log(__dirname);
+  app.use(express.static(path.join(__currentPath, '/client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__currentPath, 'client', 'build', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('Hello world');
+  });
+}
 
 module.exports = app;
